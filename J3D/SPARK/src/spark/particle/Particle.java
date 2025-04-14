f99733f5803b029629cf5b6c8a0432c201560113
NAME <mail@gmail.com>
2025-04-14 10:25:34 +0200

    anges

---
+++ J3D/SPARK/src/spark/particle/Particle.java
@@ -0,0 +1,17 @@
+//***************************************************************************
+package spark.particle;
+//***************************************************************************
+
+
+//***************************************************************************
+public class Particle {
+    
+    public int generation;
+    public int children;
+    public Vector3f location;
+    public Vector3f velocity;
+
+    public Particle() {
+    }
+
+ }
