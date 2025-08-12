71ab9241ef7315156d8c2f4a088aa89338189080
Philip Reichel <philip@DEBIANPR.fritz.box>
2025-08-13 00:35:43 +0200

    Updates: actuators.scad, ROBARM-V01

--- Arduino/ROBARM-V01/ROBARM-V01.ino
+++ Arduino/ROBARM-V01/ROBARM-V01.ino
@@ -4,156 +4,222 @@
 #include <Adafruit_PWMServoDriver.h>
 //==============================================================================
 
-//==============================================================================
-const int SHOULDER = 0x04;
-const int ELLBOW   = 0x08;
-const int WRIST    = 0x0C;
-//==============================================================================
 
 //==============================================================================
-AccelStepper            stp(AccelStepper::FULL4WIRE, 5, 8, 6, 7);
+const int SHOULDER       = 0x04;
+const int ELBOW          = 0x08;
+const int WRIST          = 0x0C;
+//==============================================================================
+const int ROT_A          = 5;
+const int ROT_B          = 6;
+const int ROT_C          = 7;
+const int ROT_D          = 8;
+//==============================================================================
+const float SPEED        = 3.0;
+const int   ROT_MID      = 0;
+const int   SHOULDER_MID = 375;
+const int   ELBOW_MID    = 325;
+const int   WRIST_MID    = 345;
+//==============================================================================
+
+
+//==============================================================================
+long mark;
+bool pressed = false;
+int  mode = 0;
+//==============================================================================
+int target_rotation = ROT_MID;
+int target_shoulder = SHOULDER_MID;
+int target_elbow    = ELBOW_MID;
+int target_wrist    = WRIST_MID;
+//==============================================================================
+int current_rotation = target_rotation;
+int current_shoulder = target_shoulder;
+int current_elbow    = target_elbow;
+int current_wrist    = target_wrist;
+//==============================================================================
+AccelStepper            stp(AccelStepper::FULL4WIRE, ROT_A, ROT_D, ROT_B, ROT_C);
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 //==============================================================================
 
-//==============================================================================
-float speed = 3;
-//==============================================================================
 
 //==============================================================================
-int target_shoulder = 375;
-int target_ellbow   = 325;
-int target_wrist    = 325;
-//==============================================================================
-
-//==============================================================================
-int current_shoulder = 375;
-int current_ellbow   = 325;
-int current_wrist    = 325;
-//==============================================================================
-
-//==============================================================================
-long past;
-long clock;
-//==============================================================================
-
-//==============================================================================
-void target_apply(
+void set_target(
+		int _rotation,
 		int _shoulder,
-		int _ellbow,
+		int _elbow,
 		int _wrist) {
+	target_rotation = _rotation;
 	target_shoulder = _shoulder;
-	target_ellbow   = _ellbow;
+	target_elbow   = _elbow;
 	target_wrist    = _wrist;
 };
 //==============================================================================
 
+
 //==============================================================================
-void current_apply(
+void set_pose(
+		int _rotation,
 		int _shoulder,
-		int _ellbow,
+		int _elbow,
 		int _wrist) {
 
+	current_rotation = _rotation;
 	current_shoulder = _shoulder;
-	current_ellbow   = _ellbow;
+	current_elbow    = _elbow;
 	current_wrist    = _wrist;
 	
+	/*
+	bool doRotation = (target_rotation - current_rotation) != 0;
+	bool doShoulder = (target_shoulder - current_shoulder) != 0;
+	bool doElbow    = (target_elbow    - current_elbow   ) != 0;
+	bool doWrist    = (target_wrist    - current_wrist   ) != 0;
+	
+	if (doRotation) stp.moveTo(current_rotation);
+	if (doShoulder) pwm.setPWM(SHOULDER, 0, current_shoulder);
+	if (doElbow)    pwm.setPWM(ELBOW, 0, current_elbow);
+	if (doWrist)    pwm.setPWM(WRIST, 0, current_wrist);
+	*/
+
+	stp.moveTo(current_rotation);
 	pwm.setPWM(SHOULDER, 0, current_shoulder);
-	pwm.setPWM(ELLBOW, 0, current_ellbow);
-	pwm.setPWM(WRIST, 0, current_wrist);
+	pwm.setPWM(ELBOW,    0, current_elbow);
+	pwm.setPWM(WRIST,    0, current_wrist);
 	
 };
 //==============================================================================
 
+
 //==============================================================================
 void update(float _dT) {
 
-	float dS = (target_shoulder - current_shoulder) * _dT * speed;
-	float dE = (target_ellbow   - current_ellbow) * _dT * speed;
-	float dW = (target_wrist    - current_wrist) * _dT * speed;
-	
-	current_shoulder += (int) round(dS);
-	current_ellbow   += (int) round(dE);
-	current_wrist    += (int) round(dW);
+	if (!pressed && digitalRead(3) == LOW) {
+		pressed = true;
+		update_mode();
+	} else if (pressed && digitalRead(3) == HIGH) {
+		pressed = false;
+	}
 
-	pwm.setPWM(SHOULDER, 0, current_shoulder);
-	pwm.setPWM(ELLBOW, 0, current_ellbow);
-	pwm.setPWM(WRIST, 0, current_wrist);
+	float dR = (target_rotation - current_rotation) * _dT * SPEED;
+	float dS = (target_shoulder - current_shoulder) * _dT * SPEED;
+	float dE = (target_elbow    - current_elbow)    * _dT * SPEED;
+	float dW = (target_wrist    - current_wrist)    * _dT * SPEED;
 	
+	current_rotation += (int) round(dR);
+	current_shoulder += (int) round(dS);
+	current_elbow    += (int) round(dE);
+	current_wrist    += (int) round(dW);
+	
+	set_pose(
+		current_rotation,
+		current_shoulder,
+		current_elbow,
+		current_wrist);
+
 }
 //==============================================================================
 
-//==============================================================================
-int stptarget = 1024;
-//==============================================================================
 
 //==============================================================================
 void setup() {
 	
 	Serial.begin(9600);
 
-	Serial.println("INIT START");
+	Serial.println("INIT BUTTON");
+	pressed = false;
+	mode    = 0;
+	pinMode(3, INPUT_PULLUP);
 
+	Serial.print("INITIAL MODE: ");
+	Serial.print(mode);
+	Serial.println();
+
+	Serial.println("INIT STEPPER");
 	stp.setAcceleration(1e9);
 	stp.setMaxSpeed(300);
 	stp.setSpeed(300);
-
 	stp.setCurrentPosition(0);
-	stp.moveTo(stptarget);
-	
+
+	Serial.println("INIT SERVOS");
 	pwm.begin();
 	pwm.setPWMFreq(50);
-
 	delay(500);
 
-	current_apply(
+	Serial.println("INIT POSE");
+	set_target(
+		target_rotation,
 		target_shoulder,
-		target_ellbow,
+		target_elbow,
 		target_wrist);
-
 	delay(500);
 
-	past = millis();
+	mark = millis();
 
-	Serial.println("INIT COMPLETE");
-	
 };
 //==============================================================================
 
 //==============================================================================
+void update_mode() {
+	mode = (mode+1) % 6;
+	Serial.print("NEW MODE: ");
+	Serial.print(mode);
+	Serial.println();
+	switch (mode) {
+		case 0:
+			set_target(
+				ROT_MID,
+				SHOULDER_MID,
+				ELBOW_MID,
+				WRIST_MID);
+			break;
+		case 1:
+			set_target(
+				ROT_MID + 512,
+				target_shoulder,
+				target_elbow,
+				target_wrist);
+			break;
+		case 2:
+			set_target(
+				ROT_MID - 512,
+				target_shoulder,
+				target_elbow,
+				target_wrist);
+			break;
+		case 3:
+			set_target(
+				ROT_MID,
+				SHOULDER_MID,
+				ELBOW_MID,
+				WRIST_MID + 125);
+			break;
+		case 4:
+			set_target(
+				ROT_MID,
+				SHOULDER_MID - 125,
+				ELBOW_MID - 125,
+				WRIST_MID - 125);
+			break;
+		case 5:
+			set_target(
+				ROT_MID,
+				SHOULDER_MID + 125,
+				ELBOW_MID + 125,
+				WRIST_MID + 125);
+			break;
+	};
+}
+//==============================================================================
+
+//==============================================================================
 void loop() {
-	
-	long mark = millis();
-	long delta = mark - past;
-	if (delta >= 100) {
-		clock += delta;
-		past = mark;
+	long now = millis();
+	long delta = now - mark;
+	if (delta >= 50) {
+		mark = now;
 		float dT = (float) delta / 1000.;		
-		//update(dT);
-		Serial.println("...");
+		update(dT);
 	}
-
-	/*
-	if (clock >= 2000) {
-		target_apply(500, 450, 325);
-	}
-
-	if (clock >= 4000) {
-		target_apply(250, 200, 325);
-	}
-
-	if (clock >= 6000) {
-		clock = 0;
-		target_apply(375, 325, 325);
-	}
-	
-	*/
-	
-	//stp.runSpeed();
-	if (!stp.run()) {
-		stptarget = 1024 - stptarget;
-		stp.moveTo(stptarget);
-	}
-	//yield();
-	
+	stp.run();
 }
 //==============================================================================
