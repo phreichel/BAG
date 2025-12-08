7be4b59b8f7ba655d33f705d44db1c7a74473e8c
Philip Reichel <phreichel@gmx.de>
2025-12-08 22:52:54 +0100

    Added TasterPAD code project

---
+++ Atmel/TasterPAD/TasterPAD/Debug/main.d
@@ -0,0 +1,46 @@
+main.d main.o: .././main.c \
+ c:\apps\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\xc.h \
+ c:\apps\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\cci.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\io.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\sfr_defs.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\inttypes.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\features.h \
+ c:\apps\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\stdint.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\bits\alltypes.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\bits\stdint.h \
+ C:/Apps/Atmel/Studio/7.0/Packs/atmel/ATtiny_DFP/1.10.348/xc8/avr/include/avr/iotn13a.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\portpins.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\common.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\fuse.h \
+ c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\lock.h
+
+c:\apps\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\xc.h:
+
+c:\apps\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\cci.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\io.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\sfr_defs.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\inttypes.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\features.h:
+
+c:\apps\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\stdint.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\bits\alltypes.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\bits\stdint.h:
+
+C:/Apps/Atmel/Studio/7.0/Packs/atmel/ATtiny_DFP/1.10.348/xc8/avr/include/avr/iotn13a.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\portpins.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\common.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\fuse.h:
+
+c:\apps\microchip\xc8\v2.36\avr\avr\include\avr\lock.h:
---
+++ Atmel/TasterPAD/TasterPAD/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 12/8/2025 9:23:47 PM
+ *  Author: phrei
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
